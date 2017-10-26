e4aecd737f4a829442e9249b804baf9ab07fcccd
Robbie <txt3rob@gmail.com>
2017-10-26 22:40:15 +0100

    Update redirect.py
    added special page to confirm

--- redirect.py
+++ redirect.py
@@ -19,7 +19,7 @@ def test_redirect(url,payload):
 	headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	exmp = url.replace("https://","")
 	exmp = exmp.replace("http://","")
-	payload = payload.replace("example.com",""+exmp+"")
+	payload = payload.replace("example.com","redirect.xsses.rocks")
 	URL = ""+url+""+payload+""
 	response = session.get(URL, headers=headers, verify=False)
 	try:
@@ -39,6 +39,13 @@ def test_redirect(url,payload):
 				file = open("found.txt","a") 
 				file.write(""+URL+"\n")
 				file.close() 
+		if 'fc38f65c089c19c2c261c3d1e92c8e76' in response.content:
+			print "\n\n\n\n\n\n[*]*****Open Redirect Found*****[*]"
+			print "[*]"+URL+"[*]"
+			print "\n\n\n\n"
+			file = open("found.txt","a") 
+			file.write(""+URL+"\n")
+			file.close() 
 
 
 		else:
