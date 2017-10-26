66fdf39cfcb4b80ada6436cb941a64a6b0b84a29
Robbie <txt3rob@gmail.com>
2017-10-26 16:22:10 +0100

    Update redirect.py

--- redirect.py
+++ redirect.py
@@ -17,8 +17,8 @@ payloadlist = sys.argv[2]
 def test_redirect(url,payload):
 
 	headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
-	exmp = url.replace("https://")
-	exmp = exmp.replace("http://")
+	exmp = url.replace("https://","")
+	exmp = exmp.replace("http://","")
 	payload = payload.replace("example.com",""+exmp+"")
 	URL = ""+url+""+payload+""
 	response = session.get(URL, headers=headers, verify=False)
