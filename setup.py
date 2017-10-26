3e194c8320c280697343bb7daf52cdc14be80380
Robbie <txt3rob@gmail.com>
2017-10-26 15:43:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,71 +1,16 @@
-# -*- coding: utf-8 -*-
-
-# MIT License
-#
-# Copyright (c) 2017 Robbie Wiggins
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-import re
-
-from setuptools import find_packages, setup
+import setuptools
+from packagename.version import Version
 
 
-with open("requirements.txt") as file:
-    requirements = file.read().splitlines()
-
-
-
-setup(
-    name=PackageHelper.get_alias(),
-    version=PackageHelper.get_version(),
-    description=PackageHelper.get_description(),
-    long_description=readme,
-    keywords = ["vulnerability", "bug-bounty", "security", "openredirect", "scanner"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Security"
-    ],
-    entry_points = {
-        'console_scripts': [
-            'redirect = redirect:main'
-        ]
-    },
-    platforms=["any"],
-    author="Robbie Wiggins",
-    author_email="txt3rob@gmail.com",
-    license="MIT",
-    url="https://github.com/random-robbie/open-redirect",
-    install_requires=requirements
-)
+setuptools.setup(name='Open Redirect Scanner',
+                 version=Version('1.0.0').number,
+                 description='Open Redirect Scanner',
+                 author='Random_Robbie',
+                 author_email='txt3rob@gmail.com',
+                 url='http://path-to-my-packagename',
+                 platforms='any',
+                 install_requires=requirements,
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='Open Redirect Scanner',
+                 classifiers=['Packages', 'Open Redirect Scanner'])
