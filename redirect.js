7f571c1862d4dc5493690e3011a7c1d183238521
Robbie <txt3rob@gmail.com>
2017-10-27 14:55:10 +0100

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,23 @@
+var title = [];
+var casper = require('casper').create({
+    pageSettings: {
+        loadImages: false,//The script is much faster when this field is set to false
+        loadPlugins: false,
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+    }
+});
+ 
+
+function getTitle() {
+    var title = document.querySelectorAll('your-selector');
+    return Array.prototype.map.call(title, function(a) {
+        return a.getAttribute('title');
+    });
+};
+casper.start(casper.cli.get(0), function() {
+     this.echo('' + this.getCurrentUrl(), 'COMMENT');
+ 
+});
+ 
+ 
+casper.run();
